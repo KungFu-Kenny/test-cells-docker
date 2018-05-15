a97d318348cc7fe8ce8a79c43da73fc855b1d7db
kungfu-kenny <jihad.thabet@gmail.com>
2018-05-15 13:04:47 +0200

    updates

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-bootstrapjson=~/.config/pydio/cells/static/pydio/data/plugins/boot.conf/bootstrap.json
-
 sudo service php7.2-fpm start
 sudo service php7.2-fpm status
 
@@ -9,7 +7,7 @@ sudo sed -i -e "s/listen.owner\s*=\s*www-data/listen.owner = cells/g" /etc/php/7
 sudo sed -i -e "s/listen.group\s*=\s*www-data/listen.group = cells/g" /etc/php/7.2/fpm/pool.d/www.conf
 
 sudo service php7.2-fpm restart
-
+sudo service php7.2-fpm status
 
 FILE=/home/cells/.config/pydio/cells/pydio.json
 if [ ! -f $FILE ] ; then
