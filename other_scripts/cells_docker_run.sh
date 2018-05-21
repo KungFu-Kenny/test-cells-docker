f5a3b5a0442efb72c38f0717820a9d79999dd336
kungfu-kenny <jihad.thabet@gmail.com>
2018-05-21 12:13:17 +0200

    up

--- other_scripts/cells_docker_run.sh
+++ other_scripts/cells_docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it -d --name cells -t -v /home/tmp/test/:/home/cells/.config/pydio/cells/ -e "CELLS_BIND=$1:$2" -e "CELLS_EXTERNAL=$1:$2" -p $2:$2 cells
+docker run -it -d --name cells -t -v /home/tmp/test/:/home/cells/.config/pydio/cells/ -e "CELLS_BIND=$ADDR:$PORT" -e "CELLS_EXTERNAL=$ADDR:$PORT" -p $PORT:$PORT cells
